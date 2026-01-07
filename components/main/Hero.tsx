8640414d158e6dcbbf39cb22a3b4874ffa92af8f
ravindu14 <ravindulandekumbura14@gmail.com>
2026-01-07 16:17:58 +1100

    video playing issue in mobile

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,7 +8,9 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="absolute left-0 z-1 w-full h-full object-cover"
+        playsInline
+        preload="metadata"
+        className="absolute inset-0 z-0 h-full w-full object-cover"
       >
         <source src="/header-banner.mp4" type="video/mp4" />
       </video>
